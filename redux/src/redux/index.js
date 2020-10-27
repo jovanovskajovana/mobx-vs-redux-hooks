d49081937b10cbe78d5f0ff40387fd5248d9cd78
jovanovskajovana <jovanovska.jovana1@gmail.com>
2020-10-27 13:28:38 +0100

    Add redux devtools

--- redux/src/redux/index.js
+++ redux/src/redux/index.js
@@ -8,6 +8,9 @@ const rootReducer = combineReducers({
   owner,
 })
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
